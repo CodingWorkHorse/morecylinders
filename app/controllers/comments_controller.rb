567e580250fa90e0d2ac5dbc18b1a6cca5b2f457
CodingWorkHorse <codingworkhorse@gmail.com>
2019-10-12 15:07:03 -0500

    Water Like A Boat

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -28,7 +28,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to listing_path(@comment.listing_id), notice: 'Comment was successfully created.' }
+        format.html { redirect_to listing_path(@comment.listing_id), :id => "comment_section_destination" , notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
